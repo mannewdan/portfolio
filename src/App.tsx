f2b4f20318b4d00c2b03d066af7ab457c4710420
mannewdan <109920968+mannewdan@users.noreply.github.com>
2023-05-05 03:07:42 -0500

    typography

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,18 @@ export default function App() {
     <>
       <Header />
       <Hero />
-      <h1>Totam eius eos perspiciatis</h1>
-      <h2>Totam eius eos perspiciatis</h2>
-      <h3>Totam eius eos perspiciatis</h3>
-      <p style={{ height: "200vh" }}>
+      <h1 className="text-h-l">Totam eius eos perspiciatis</h1>
+      <h2 className="text-h-m">Totam eius eos perspiciatis</h2>
+      <h3 className="text-h-s">Totam eius eos perspiciatis</h3>
+      <p className="text-b-m">
         Nulla quis earum corrupti consectetur consectetur. Mollitia at vel
         dolorem aliquid sapiente incidunt. Ut exercitationem ad fugit. Iusto
-        tenetur pariatur nisi architecto sint labore est voluptatem. Ea eos modi
-        veniam doloribus nesciunt. Iste quia consequatur qui sit enim. Eveniet
-        ea saepe numquam dolores quia libero illo. Molestias tenetur quae
-        consequuntur enim beatae laborum occaecati incidunt. Totam ut
-        exercitationem rerum. Blanditiis asperiores maxime aperiam debitis eum
-        quasi beatae autem. Eligendi quia quaerat voluptatem nisi ut expedita
-        est. Doloribus quibusdam reiciendis ullam sequi tempore. Pariatur
-        molestias dolore eveniet impedit voluptas et. Qui tempora culpa
-        incidunt. Earum ipsa dolorum aut molestias architecto consequatur. Iusto
-        aut labore error similique nostrum sequi. Aut est quia deleniti ipsa aut
-        placeat. Rerum expedita autem et ipsa debitis. Nihil sunt dolore quo
-        rerum recusandae non voluptas nesciunt. Sunt aliquid ut ut itaque quia.
-        A quam voluptas aut est.
+        tenetur pariatur nisi architecto sint labore est voluptatem.
+      </p>
+      <p className="text-b-s" style={{ height: "200vh" }}>
+        Nulla quis earum corrupti consectetur consectetur. Mollitia at vel
+        dolorem aliquid sapiente incidunt. Ut exercitationem ad fugit. Iusto
+        tenetur pariatur nisi architecto sint labore est voluptatem.
       </p>
       <div id="projects" className="scroll-destination"></div>
       <p>Scroll To Here</p>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,15 @@ export default function Header() {
   return (
     <>
       <header className="header header-background">
-        <span>Daniel Newman</span>
+        <span className="text-h-s">Daniel Newman</span>
         <nav>
-          <a href="#home" className="c-text-light-50">
+          <a href="#home" className="text-b-m c-text-light-50">
             Home
           </a>
-          <a href="#projects" className="c-text-light-50">
+          <a href="#projects" className="text-b-m c-text-light-50">
             Projects
           </a>
-          <a href="#contact" className="c-text-light-50">
+          <a href="#contact" className="text-b-m c-text-light-50">
             Contact
           </a>
         </nav>
