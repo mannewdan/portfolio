99b217174f344bafa298696fe5094a0094559579
mannewdan <109920968+mannewdan@users.noreply.github.com>
2023-05-06 20:30:05 -0500

    background animation

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -2,10 +2,12 @@ export default function Background() {
   return (
     <>
       <div className="background-animation">
-        <div className="shape-1"></div>
-        <div className="shape-2"></div>
-        <div className="shape-3"></div>
-        <div className="shape-4"></div>
+        <div className="pivot">
+          <div className="shape shape-1"></div>
+          <div className="shape shape-2"></div>
+          <div className="shape shape-3"></div>
+          <div className="shape shape-4"></div>
+        </div>
       </div>
     </>
   );
