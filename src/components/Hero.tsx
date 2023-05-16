955baf949879ab2d67ca07ff8d4df475abe9dac2
mannewdan <109920968+mannewdan@users.noreply.github.com>
2023-05-16 04:39:47 -0500

    responsive text size improvements

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,8 +5,10 @@ export default function Hero() {
     <section className="hero">
       <Background />
       <div className="main-container">
-        <h1 className="text-h-l uppercase">Hey! I'm Dan</h1>
-        <p className="text-h-m c-text-light-75 uppercase">Frontend Developer</p>
+        <h1 className="text-h-l uppercase greeting">Hey! I'm Dan</h1>
+        <p className="text-h-m c-text-light-75 uppercase title">
+          Frontend Developer
+        </p>
 
         <a href="#projects" className="text-h-s">
           See my work
