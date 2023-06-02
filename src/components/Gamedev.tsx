68cb912ae1248b7b1fff13df3b0766327c720bc1
mannewdan <109920968+mannewdan@users.noreply.github.com>
2023-06-01 23:36:15 -0500

    video play & pause effects

--- src/components/Gamedev.tsx
+++ src/components/Gamedev.tsx
@@ -1,22 +1,63 @@
+import { useState } from "react";
 import Video from "./Video";
+import data from "../gamedevdata.json";
+
+export type VideoT = {
+  id: number;
+  label: string;
+  url: string;
+  isPlaying: boolean;
+  lastClicked: boolean;
+};
 
 export default function Gamedev() {
+  const [videos, setVideos] = useState<Array<VideoT>>(
+    data.map((item) => {
+      return { ...item, isPlaying: false, lastClicked: false };
+    })
+  );
+
+  function pauseAll(exceptForThisID: number) {
+    setVideos((prev) => {
+      return prev.map((item) => {
+        if (item.id === exceptForThisID) {
+          return item;
+        } else return { ...item, isPlaying: false, lastClicked: false };
+      });
+    });
+  }
+  function toggleIsPlaying(id: number) {
+    setVideos((prev) => {
+      return prev.map((item) => {
+        if (item.id === id) {
+          return { ...item, isPlaying: !item.isPlaying, lastClicked: true };
+        } else return item;
+      });
+    });
+  }
+
+  //rendering
+  const videoEls = videos.map((item) => {
+    return (
+      <Video
+        key={item.id}
+        video={item}
+        onClick={() => {
+          toggleIsPlaying(item.id);
+          pauseAll(item.id);
+        }}
+      />
+    );
+  });
   return (
     <section className="gamedev alt">
       <h2 className="text-h-m section-title">Other Works</h2>
       <p className="description text-b-s c-text-medium padded-container">
-        A look at some of the things I've created in the Unity game engine
+        A look at some other things I've done in my free time
       </p>
 
       <div className="padded-container">
-        <div className="grid">
-          <Video url="gamedev/clouds.webm" />
-          <Video url="gamedev/clouds.webm" />
-          <Video url="gamedev/clouds.webm" />
-          <Video url="gamedev/clouds.webm" />
-          <Video url="gamedev/clouds.webm" />
-          <Video url="gamedev/clouds.webm" />
-        </div>
+        <div className="grid">{videoEls}</div>
       </div>
     </section>
   );
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,34 +1,56 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
+import { VideoT } from "./Gamedev";
+import Icon from "./Icon";
 
 type VideoProps = {
-  url: string;
+  video: VideoT;
+  onClick: () => void;
 };
 
-export default function Video({ url }: VideoProps) {
-  const [isPlaying, setIsPlaying] = useState(false);
+export default function Video({ video, onClick }: VideoProps) {
+  const [playedOnce, setPlayedOnce] = useState(false);
   const videoRef = useRef<HTMLVideoElement>(null);
 
-  return (
-    <video
-      onClick={() => {
-        setIsPlaying((prev) => {
-          const nowPlaying = !prev;
-          if (nowPlaying) {
-            videoRef.current?.play();
-          } else {
-            videoRef.current?.pause();
-          }
+  useEffect(() => {
+    if (video.isPlaying) {
+      videoRef.current?.play();
+    } else {
+      videoRef.current?.pause();
+    }
+  }, [video.isPlaying]);
 
-          return nowPlaying;
-        });
-      }}
-      ref={videoRef}
-      controls={false}
-      autoPlay={false}
-      muted={true}
-      loop={true}
-    >
-      <source type="video/webm" src={url}></source>
-    </video>
+  return (
+    <div className="video-container">
+      <video
+        onClick={() => {
+          setPlayedOnce(true);
+          onClick();
+        }}
+        ref={videoRef}
+        controls={false}
+        autoPlay={false}
+        muted={true}
+        loop={true}
+      >
+        <source type="video/webm" src={video.url}></source>
+      </video>
+      <p className="label text-h-s">{video.label}</p>
+
+      {!playedOnce && (
+        <div className="indicator play">
+          <Icon url="play.svg" />
+        </div>
+      )}
+      {playedOnce && video.lastClicked && video.isPlaying && (
+        <div className="indicator play pulse-fade">
+          <Icon url="play.svg" />
+        </div>
+      )}
+      {playedOnce && video.lastClicked && !video.isPlaying && (
+        <div className="indicator pause pulse-fade">
+          <Icon url="pause.svg" />
+        </div>
+      )}
+    </div>
   );
 }
